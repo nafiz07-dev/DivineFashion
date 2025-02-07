9e4fd3d28f1f81be0f371e896d643f13e1aceade
Nafiz <nafiz07.dev@gmail.com>
2025-02-08 03:16:27 +0600

    redeployed slider js

--- dist/index.253c89eb.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.253c89eb.js.map
--- dist/shop/shop.253c89eb.js
+++
@@ -1,9 +0,0 @@
-// import { controllNav } from "./frontend";
-// const controllNav = function () {
-//     document.querySelector('.navbar__actions--navlinks').addEventListener('click', function () {
-//         console.log('clicked');
-//     })
-// }
-controllNav();
-
-//# sourceMappingURL=shop.253c89eb.js.map
