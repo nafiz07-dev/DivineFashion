6f7cb22d306a790e7018ebe5efa5a3f611ecdd1e
Nafiz Al Rakib <152012983+nafiz07-dev@users.noreply.github.com>
2025-03-09 16:32:47 +0600

    Delete src directory

--- src/search.js
+++
@@ -1,7 +0,0 @@
-import * as vars from './vars.js';
-
-export const redirectToShop = function () {
-  vars.searchSection.addEventListener('click', function () {
-    vars.searchSection.setAttribute('href', '/shop/shop.html#search');
-  });
-};
